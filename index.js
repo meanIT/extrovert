c5a9ce000262961e1ddf5d3d16cc91df07cc48ab
Valeri Karpov <val@karpov.io>
2022-09-08 14:13:51 -0400

    feat: add applySpec

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 exports.pureMiddleware = require('./src/util/pureMiddleware');
 exports.registerExtrovertModels = require('./src/db').registerExtrovertModels;
 exports.toRoute = require('./src/util/toRoute');
+exports.applySpec = require('./src/util/applySpec');
---
+++ src/util/applySpec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = applySpec;
+
+function applySpec(obj) {
+  const ret = {};
+  for (const [key, value] of Object.entries(obj)) {
+    if (typeof value === 'function') {
+      ret[key] = value.apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (value != null && typeof value === 'object') {
+      ret[key] = applySpec.apply(this, [obj[key]].concat(Array.prototype.slice.call(arguments, 1)));
+    }
+  }
+
+  return ret;
+};

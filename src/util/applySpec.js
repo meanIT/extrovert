ef8276a0e75b726245ab0401276f118f87994479
Valeri Karpov <val@karpov.io>
2022-11-29 12:10:35 -0500

    make applySpec pull function properties

--- src/util/applySpec.js
+++ src/util/applySpec.js
@@ -6,9 +6,14 @@ function applySpec(obj) {
   const ret = {};
   for (const [key, value] of Object.entries(obj)) {
     if (typeof value === 'function') {
-      ret[key] = value.apply(this, Array.prototype.slice.call(arguments, 1));
+      ret[key] = Object.assign(
+        value.apply(this, Array.prototype.slice.call(arguments, 1)),
+        value
+      );
     } else if (value != null && typeof value === 'object') {
       ret[key] = applySpec.apply(this, [obj[key]].concat(Array.prototype.slice.call(arguments, 1)));
+    } else {
+      ret[key] = value;
     }
   }
 
---
+++ test/applySpec.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const applySpec = require('../src/util/applySpec');
+const assert = require('assert');
+
+describe('applySpec', function() {
+  it('applies function properties to result', function() {
+    const obj = {
+      fn: () => getAnswer
+    };
+    obj.fn.hideFromAPI = true;
+
+    const res = applySpec(obj);
+    assert.strictEqual(res.fn.hideFromAPI, true);
+    assert.strictEqual(res.fn, getAnswer);
+
+    function getAnswer() { return 42; }
+  });
+});

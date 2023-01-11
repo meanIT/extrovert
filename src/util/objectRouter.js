1174de867cc804a1cbe368817eefe979b8a2c3c8
Valeri Karpov <val@karpov.io>
2023-01-11 12:51:04 -0500

    allow transforming route paths for kebab case etc

--- src/util/objectRouter.js
+++ src/util/objectRouter.js
@@ -2,27 +2,29 @@
 
 const express = require('express');
 
-module.exports = function createObjectRouter(obj, transformRoute, base) {
+module.exports = function createObjectRouter(obj, transformRoute, options, base) {
   if (obj == null) {
     return null;
   }
 
   const router = express.Router();
   transformRoute = transformRoute || noop;
+  const formatRoutePath = options && options.formatRoutePath || (v => `/${v}`);
 
   for (const [key, value] of Object.entries(obj)) {
-    const fullPath = base ? `${base}/${key}` : key;
+    const formattedKey = formatRoutePath(key, base);
+    const fullPath = base ? `${base}${formattedKey}` : formattedKey;
     if (typeof value === 'function') {
-      router.options(`/${key}`, (req, res) => res.send(''));
-      router.get(`/${key}`, transformRoute(value, fullPath));
-      router.put(`/${key}`, transformRoute(value, fullPath));
-      router.post(`/${key}`, transformRoute(value, fullPath));
-      router.delete(`/${key}`, transformRoute(value, fullPath));
+      router.options(formattedKey, (req, res) => res.send(''));
+      router.get(formattedKey, transformRoute(value, fullPath));
+      router.put(formattedKey, transformRoute(value, fullPath));
+      router.post(formattedKey, transformRoute(value, fullPath));
+      router.delete(formattedKey, transformRoute(value, fullPath));
       continue;
     }
 
     if (value != null && typeof value === 'object') {
-      router.use(`/${key}`, createObjectRouter(value, transformRoute, fullPath));
+      router.use(formattedKey, createObjectRouter(value, transformRoute, options, fullPath));
     }
   }
 
